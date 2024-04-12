4213eb26f1e1fb3f159212aeb3dfea8cb5f0b986
ashiarora6 <aashiarora6666@gmail.com>
2024-04-12 12:48:17 +0530

    Created Building, it 'has-a' Floor, and that's how it will wrap together

---
+++ Building.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Building {
+    List<Floor> floorNumber;
+    String BuildingName;
+
+    Building(String BuildingName){
+        this.BuildingName = BuildingName;
+    }
+
+    
+}
--- Floor.java
+++ Floor.java
@@ -18,5 +18,15 @@ public class Floor {
         meetingRooms.add(new Room(roomName));
         System.out.println("The room" + roomName + "has been successfully been added as MeetingRoom");
     }
+
+    public Room getRoom(String roomName){
+        for(Room room: meetingRooms){
+            if(room.roomName == roomName){
+                return room;
+            }
+        }
+        System.out.println("The room doesn't exist you will have to create the Room : " + roomName);
+        return null;
+    }
     
 }
