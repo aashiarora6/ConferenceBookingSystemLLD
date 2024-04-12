315e8d57b66e9ca565f4bfcd7e771ce5bca86f09
ashiarora6 <aashiarora6666@gmail.com>
2024-04-12 12:55:03 +0530

    Added two functions AddFloor and getFloor, here I have assumed that you can add ramdom Floors not in increasing order

--- Building.java
+++ Building.java
@@ -1,12 +1,29 @@
 import java.util.*;
 
 public class Building {
-    List<Floor> floorNumber;
-    String BuildingName;
+    List<Floor> listOfFloor;
+    String buildingName;
 
     Building(String BuildingName){
-        this.BuildingName = BuildingName;
+        this.buildingName = BuildingName;
+    }
+    public void addFloor(Floor temp){
+        for (Floor f : listOfFloor) {
+            if (f.floorNumber == temp.floorNumber) {
+                System.out.println("Floor is already there");
+                return;
+            }
+        }
+        listOfFloor.add(temp);
+        System.out.println("Floor number " + temp.floorNumber + " successfully added in the building " + buildingName);
     }
 
-    
+    public Floor getFloor(int floorNumber){
+        for (Floor f : listOfFloor) {
+            if (f.floorNumber == floorNumber)
+                return f;
+        }
+        return null;
+    }
+
 }
